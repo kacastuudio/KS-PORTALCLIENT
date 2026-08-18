b5c072e5a9c947a8b4053503b8c759bc02ac8231
kacastuudio <kacastuudio.id@gmail.com>
2026-08-18 12:32:51 +0800

    Update pengaturan.js

--- pengaturan.js
+++ pengaturan.js
@@ -8,6 +8,6 @@ const CONFIG_APP = {
     namaVendor: "KACA STUUDIO",
 
     // 2. Google Drive API Key (Penting agar galeri bisa memuat foto)
-    googleApiKey: "AIzaSyAZczbLeVTXl-QKqLDJFWQLCd-lW9jqqxo",
+    googleApiKey: "AIzaSyDS8t9iKujAQc1PHudo7RpiIVLttA4CwPo",
     whatsappAdmin: "6281284120679" // <-- TAMBAHKAN BARIS INI (Ganti dengan nomor WA admin)
 };
